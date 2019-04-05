9feb7d986500659281aa112a380c45dae234c1ca
Lucas Mallmann <lucasmallmann76@gmail.com>
2019-04-04 22:24:17 -0300

    Tempalte engines ready

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -13,7 +18,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-})
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
 
 app.listen(3000);

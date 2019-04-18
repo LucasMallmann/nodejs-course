8021355075e1070968c9db4dfaf6fdef440cd5cd
Lucas Mallmann <lucasmallmann76@gmail.com>
2019-04-17 21:07:41 -0300

    Implementing the cart logic

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
+
+const port = 3333;
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -17,8 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.get404);
 
-app.listen(3000);
+console.log(`Server is listening on port: ${port}`);
+app.listen(port);
---
+++ models/cart.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+const path = require("path");
+
+const filePath = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "cart.json"
+);
+
+module.exports = class Cart {
+  /**
+   * Add a product to the cart
+   * @param {string} id
+   * @param {float} totalPrice
+   */
+  static addProduct(id, totalPrice) {
+    let cart = { products: [], totalPrice: 0 };
+
+    // Fetch the previous cart
+    fs.readFile(filePath, (err, fileContent) => {
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+      // Analyze the cart...check if product already exists
+      const existingProductIndex = cart.products.findIndex(
+        prod => prod.id === id
+      );
+      const existingProduct = cart.products[existingProductIndex];
+
+      let updatedProduct;
+
+      if (existingProduct) {
+        updatedProduct = { ...existingProduct };
+        updatedProduct.quantity = updatedProduct.quantity + 1;
+        cart.products = [...cart.products];
+        cart.products[existingProductIndex] = existingProduct;
+      } else {
+        updatedProduct = { id: id, quantity: 1 };
+        cart.products = [...cart.products, updatedProduct];
+      }
+
+      // Update the totalPrice
+      cart.totalPrice = parseFloat(cart.totalPrice) + parseFloat(totalPrice);
+
+      fs.writeFile(filePath, JSON.stringify(cart), err => {
+        if (err) console.log(err);
+      });
+    });
+  }
+};
